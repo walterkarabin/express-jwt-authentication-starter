8a53ef3a8188baa789a37ace23aba4b489310561
Walter Karabin <walter.karabin@gmail.com>
2023-10-25 16:10:14 -0700

    complete the passport jwt tutorial

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,63 @@ const passport = require('passport');
 const utils = require('../lib/utils');
 
 // TODO
-router.get('/protected', (req, res, next) => {
+router.get('/protected', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+    res.status(200).json({success: true, msg: 'You are successfully authenticated to this route!'});
 });
 
 // TODO
-router.post('/login', function(req, res, next){});
+router.post('/login', function(req, res, next){
+    User.findOne({username: req.body.username})
+    .then((user) => {
+        if(!user){
+            res.status(401).json({success: false, msg: 'Could not find user.'});
+        }
+        const isValid = utils.validPassword(req.body.password, user.hash, user.salt);
+
+        if(isValid){
+            const tokenObject = utils.issueJWT(user);
+
+            res.status(200).json({success: true, msg: 'Logged In Successfully!', user: user, token: tokenObject.token, expiresIn: tokenObject.expires});
+        } else {
+            res.status(401).json({success: false, msg: 'You entered the wrong password.'});
+        }
+    });
+});
 
 // TODO
-router.post('/register', function(req, res, next){});
+router.post('/register', function(req, res, next){
+    User.findOne({username: req.body.username})
+    .then((user) => {
+        if(user){
+            res.json({success: false, msg: 'Username already exists.'});
+        }
+        else{
+
+            const saltHash = utils.genPassword(req.body.password);
+
+            const salt = saltHash.salt;
+            const hash = saltHash.hash;
+
+            const newUser = new User({
+                username: req.body.username,
+                hash: hash,
+                salt: salt
+            });
+
+            newUser.save()
+            .then((user) => {
+                    const jwt = utils.issueJWT(user);
+
+                    console.log(user);
+                    res.json({success: true, msg: 'Successful created new user.', user: user, token: jwt.token, expiresIn: jwt.expires});
+            })
+            .catch(err => {
+                    console.log(err);
+                    res.json({success: false, msg: 'Error creating new user.'});
+                    next(err);
+            });
+        }
+    });
+});
 
 module.exports = router;
